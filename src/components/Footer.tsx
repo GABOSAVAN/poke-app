ed9e127f4fbae030a8d5f21c9473b3630e21f483
Gabriel Navas <gnavas@senta.cl>
2025-06-05 03:14:19 -0400

    logo
    logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
       <footer className="flex flex-col gap-6 px-5 py-10 text-center @container">
         <p className="text-[#9e9d47] text-base font-normal leading-normal">
-          PokeApp
+          PokeApp by Gabriel Navas
         </p>
       </footer>
   );
